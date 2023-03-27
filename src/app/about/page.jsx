a302edb66d98b7b3df1709d66e96291b33c26bc9
Vitalii Shuplat <kicker198686@gmail.com>
2023-03-27 14:31:43 +0300

    adding about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="font-medium text-amber-500 text-2xl">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs.
+      </p>
+      <p>
+        Our website is designed to provide you with a comprehensive database of
+        movies from all around the world, along with the latest news, reviews,
+        and trailers. Our movie database is constantly updated with new
+        releases, ensuring that you have access to the latest and greatest in
+        the world of cinema. You can search for movies by title, director,
+        actor, genre, or release date, making it easy to find the perfect movie
+        for any occasion.
+      </p>
+      <p>
+        In addition to our extensive movie database, we also offer a platform
+        for movie lovers to connect and share their thoughts on the latest
+        releases. Our community section includes a forum where you can discuss
+        your favorite films with like-minded individuals and read reviews and
+        ratings from other users. We also have a section dedicated to movie news
+        and trailers, keeping you updated with the latest happenings in the
+        world of cinema. Thank you for visiting our website and we hope you
+        enjoy your time browsing through our movie database. If you have any
+        feedback or suggestions, please feel free to contact us. We are always
+        looking for ways to improve and enhance the user experience on our
+        website.
+      </p>
+      <p>
+        At our movie database website, we are dedicated to bringing you the best
+        possible movie-watching experience. Whether you are a casual viewer or a
+        hardcore film buff, you will find everything you need to satisfy your
+        cravings for great cinema. Our website is designed to be easy to
+        navigate, with intuitive search tools and helpful filters that make it
+        simple to find the movies you want to watch. And with our constantly
+        updated database, you will always be up-to-date on the latest releases
+        and hottest trends in the movie industry. But our website is more than
+        just a place to find and watch movies. It is also a community of
+        passionate movie lovers, sharing their thoughts and opinions on the
+        films they love. From lively discussions to in-depth reviews, our forum
+        is the perfect place to connect with like-minded individuals and explore
+        the world of cinema together. So whether you are in the mood for a
+        classic Hollywood blockbuster or an indie art film, we have got you
+        covered. Visit our movie database website today and start exploring the
+        wonderful world of cinema!
+      </p>
+      <p>Happy browsing!</p>
+    </div>
+  );
+}
